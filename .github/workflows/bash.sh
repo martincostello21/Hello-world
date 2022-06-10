57613055cd9025277c63b70b07f34aea83c918ae
Martin Costello <martin@foghornconsulting.com>
2022-06-10 16:54:03 -0400

    added additional template workflow

--- .github/workflows/bash.sh
+++ .github/workflows/bash.sh
@@ -4,9 +4,9 @@ region=$1
 if [[ $region == "US" ]]; then
     echo $region THIS IS THE region of US
 fi
-if [[ $region == "EU" ]]; then
+elif [[ $region == "EU" ]]; then
     echo $region THIS IS THE region of EU
 fi
-if [[ $2 == "heads/ref/somebranchname" ]]; then
+else [[ $2 == "heads/ref/somebranchname" ]]; then
     echo "I'm on the somebranchname branch"
 fi
