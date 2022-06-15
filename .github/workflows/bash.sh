1229bd71fefbd844afebb9a952ef598bed237bf9
Martin Costello <martin@foghornconsulting.com>
2022-06-15 12:58:16 -0400

    one of these days I should commit myself

--- .github/workflows/bash.sh
+++ .github/workflows/bash.sh
@@ -4,9 +4,9 @@ region=$1
 if [[ $region == "US" ]]; then
     echo $region THIS IS THE region of US
 fi
-elif [[ $region == "EU" ]]; then
+if [[ $region == "EU" ]]; then
     echo $region THIS IS THE region of EU
 fi
-else [[ $2 == "heads/ref/somebranchname" ]]; then
+if [[ $2 == "heads/ref/somebranchname" ]]; then
     echo "I'm on the somebranchname branch"
 fi
